929f0b484d40ac8818bb26034a1c01f01d3f4d1d
IJUHUH <bluedream224@naver.com>
2023-03-19 12:27:15 +0900

    메모

---
+++ Java01/src/main/java/operator/Type2.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class Type2 {
+public static void main(String[] args) {
+	// 변수를 활용해서
+	// 이름, 출생년도, 키
+	
+	// 예시의 000을 변수로 사용
+	/*
+	 이름 : 000
+	 출생년도 : 0000년
+	 키 : 000.00cm
+	 */
+	
+	String name = "홍길동";
+	int birth = 2000;
+	double height = 175.5;
+
+	System.out.println("이름 : "+name);
+	System.out.println("출생년도 : +"birth+"년");
+	System.out.println("키 : "+height+"cm");	
+	}
+}
---
+++ Java02/Java02/Java02/src/main/java/condition/Ifelse3.java
@@ -0,0 +1,20 @@
+package condition;
+
+import java.util.Scanner;
+
+public class Ifelse3 {
+		public static void main(String[] args) {
+			Scanner sc= new Scanner(System.in);
+			// 변수 = sc. nextInt();
+			
+			// 1. 20세 이상이면 성인, 14~20세는 청소년, 14세 미만은 어린이로 구분
+			
+			// 2. 학점 계산(90이상은 A, 80이상은 B, 70이상은 C, 그 미만은 F)
+			
+			// 3. 비밀번호를 정해놓고 사용자가 비밀번호를 입력해서 맞추면 '비밀번호가 맞습니다.' 
+			// 틀리면 '비밀번호가 다릅니다.' (비밀번호는 정수)
+			
+			
+			
+		}
+}
